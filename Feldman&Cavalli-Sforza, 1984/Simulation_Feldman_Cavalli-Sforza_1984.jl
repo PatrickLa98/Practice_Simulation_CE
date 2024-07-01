4f6a9c7b03845cbc8cd0aed942511558ae40a09f
Patrick Lauer <91125326+PatrickLa98@users.noreply.github.com>
2024-07-01 17:21:49 +0200

    wrote down equations for vertical transmission without selection

--- Feldman&Cavalli-Sforza, 1984/Simulation_Feldman_Cavalli-Sforza_1984.jl
+++ Feldman&Cavalli-Sforza, 1984/Simulation_Feldman_Cavalli-Sforza_1984.jl
@@ -1,8 +1,8 @@
-## Numerical Simulation based on mathematical models in Cultural and biological evolutionary processes: Gene-culture disquilibrium, Feldman and Cavalli-Sforza, 1983
+## Numerical Simulation based on mathematical models in Cultural and biological evolutionary processes: Gene-culture disquilibrium, Feldman and Cavalli-Sforza, 1984
 
 ## Goal model a dichotomous (2 variants)trait, genetic transmission diallellc haploid gene (A and a)
 ## Cultural transmission with 2 cultural states (indicated by subscripts 1 and 2):
-    ## 1.  vertical
+    ## 1. vertical
     ## 2. oblique
     ## 3. vertical with selection
 
@@ -92,8 +92,6 @@
     )
 
     ## next generation gene culture Disequilibrium
-
-
     Equation2c = function(μ, v, β1, β2, β3, β4, γ1, γ2, γ3, γ4, x1, x2, x3, x4)
       ## frequency of allele A is (used to calculte Coefficients)
       p = x1 + x2
@@ -127,4 +125,100 @@
         0.25, 0.25, 0.25, 0.25
     )
 
+    ## Convergence properties of y (frequency of cultural variant 1) and D (gene cultural Disequilibrium) over time 
+    ## can be represented by matrix U, given p (frequency of Allele A) is in equilibrium state
+    Equation3 = function(μ, v, β1, β2, β3, β4, γ1, γ2, γ3, γ4, x1, x2, x3, x4)
+        ## Coefficient (described in Appendix 1)
+        L = p*((1 - μ)*(β1 + β2 - 1) + μ*(γ1 + γ2 - 1)) +
+        (1 - p)*(v*(γ3 + γ4 - 1) + (1 - v)*(β3 + β4 - 1))
+        ## Coefficient (described in Appendix 2)
+        M = (1 - μ) * (β1 + β2 - 1) + μ*(γ1 + γ2 - 1) +
+        (1 - v) * (1 - β3 - β4) + v*(1 - γ3 - γ4)
+        ## Coefficient (described in Appendix 4)
+        R = μ*(1 - μ)*(p^2)*(β1 + β2 - γ1 - γ2) +
+        v*(1 - v)*((1 - p)^2)*(γ3 + γ4 - β3 - β4) +
+        p*(1 - p)*((1 - μ)*(1 - v)*(β1 + β2 - β3 - β4) +
+        μ*v*(γ3 + γ4 - γ1 - γ2))   
+        ## Coefficient (described in Appendix 5)
+        S = μ*(1 - μ)*p*(β1 + β2 - γ1 - γ2) + v*(1 - v)*(1 - p)*(β3 + β4 - γ3 - γ4) +
+        (1 - μ)*(1 - v)*(p*(β3 + β4 - 1) +
+        (1 - p)*(β1 + β2 - 1)) + μ*v*(p*(1 - γ3 - γ4) +
+        (1 - p)*(1 - γ1 - γ2))
+        ## Convergence Matrix U
+        U = [L M
+            R S]
+
+        return U
+    end
+
+
+   U =  Equation3(
+        0.001, 0.001,
+        0.9, 0.9, 0.9, 0.9,
+        0.9, 0.9, 0.9, 0.9,
+        0.25, 0.25, 0.25, 0.25
+    )
+
+    using LinearAlgebra
+    eigen(U)
+
+    ## The eigenvalues of U can also be represented by the roots of the quadratic Q(λ) = 0
+
+    ## Equation 5
+    λ12 = β1 + β2 - 1
+    λ34 = β3 + β4 - 1
+    γ12 = γ1 + γ2 - 1
+    γ34 = γ3 + γ4 - 1
+
+    ## Equation 4
+    Q(λ) = (λ^2) - λ*(λ12*(1 - μ) + λ34*(1 - v)) +
+            λ12*λ34*(1 - v)*(1 - μ) - γ12*γ34*μ*v
+
+    λ_seq = collect(0:0.1:1)        
+    Qs = [Q(λ) for λ in λ_seq]
+    using Plots
+    plot(λ_seq, Qs, legend = false)
+
+
+    ## Equilibrium y if no mutation occurs
+    Equation6a = function(μ, v, β1, β2, β3, β4, γ1, γ2, γ3, γ4, x1, x2, x3, x4)
+        ## if no mutation and no selection we assume p0 is p_equilibrium
+        p0 = x1 + x2
+        ## Coefficient H (Equation 6b)
+        H = (1 - β2)*(1 - β3) - (1 - β1)*(1 - β4)
+
+        equil_y = ((1 - λ12)^-1)*((1 - λ34)^-1)*((1 - β4)*(1 - λ12) + p0*H)
+        
+        return equil_y
+    end
     
+    Equation6a(
+        0.001, 0.001,
+        0.9, 0.9, 0.9, 0.9,
+        0.9, 0.9, 0.9, 0.9,
+        0.25, 0.25, 0.25, 0.25
+    )
+
+    ## Equilibrium D if no mutation occurs
+    Equation6c = function(μ, v, β1, β2, β3, β4, γ1, γ2, γ3, γ4, x1, x2, x3, x4)
+        ## if no mutation and no selection we assume p0 is p_equilibrium
+        p0 = x1 + x2
+        ## Coefficient H (Equation 6b)
+        H = (1 - β2)*(1 - β3) - (1 - β1)*(1 - β4)
+
+        equil_D = p0*(1 - p0)*H/(1 - λ12)*(1 - λ34)
+
+        return equil_D
+    end
+
+    Equation6c(
+        0.001, 0.001,
+        0.9, 0.9, 0.9, 0.9,
+        0.9, 0.9, 0.9, 0.9,
+        0.25, 0.25, 0.25, 0.25
+    )
+
+    
+
+
+
