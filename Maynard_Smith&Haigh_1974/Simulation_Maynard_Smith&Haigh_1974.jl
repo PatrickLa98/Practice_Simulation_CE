71e6486bf893661bf658c4a4b2c9781f5b6d935d
Patrick Lauer <91125326+PatrickLa98@users.noreply.github.com>
2024-07-18 13:58:40 +0200

    scribbled down plan for abm

--- Maynard_Smith&Haigh_1974/Simulation_Maynard_Smith&Haigh_1974.jl
+++ Maynard_Smith&Haigh_1974/Simulation_Maynard_Smith&Haigh_1974.jl
@@ -17,7 +17,7 @@ include("Equations_Maynard_Smith&Haigh_1974.jl")
 
 ## parameter settings
     ## Population size = n
-    N = 10^4
+    N = 10^2
     ## initial frequency of favourable allele B in population of N individuals = p0
     p0 = 1/N
     ## initial proportion of A in chromosomes containing B = Q
@@ -53,7 +53,20 @@ plot!(recombination_fractions, approx_hitchhiking_effect,
     label = "approximation")
 
 ## ABM to replicate Figure 1
-        
 
+#=
+* notation: AB = 1, Ab = 2, aB = 3, ab = 4
+* starting population: N - 1 individuls sampled as 2 or 4 (proportion dependent on R0 value)
+                     0 individuals 1
+                     1 indiviudal 3
+* individual with genotype 3 will spread and depending on strength of recombination fraction c 1 will also increase
+* run till all individuals are either 1 or 3. 
+* proportion of predefined R0 to sum 3 individuals at the end is Qinf/R0
+=#
+
+## population of N - 1 individuals with b allele(denotetd as 0)        
+pop = repeat([0], N - 1)
+## 1 individual mutates and recieves B allele
+push!(pop, 1)
 
 
