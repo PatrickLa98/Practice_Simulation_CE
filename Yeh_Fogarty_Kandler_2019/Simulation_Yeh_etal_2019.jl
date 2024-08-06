4d6fa39804dd022b83319f314d9d641391dce0ee
Patrick Lauer <91125326+PatrickLa98@users.noreply.github.com>
2024-08-06 17:03:03 +0200

    start of yeh et al replication

--- Yeh_Fogarty_Kandler_2019/Simulation_Yeh_etal_2019.jl
+++ Yeh_Fogarty_Kandler_2019/Simulation_Yeh_etal_2019.jl
@@ -1,3 +1,25 @@
 using Plots
 using Distributions
 
+
+## set up
+
+    ## individuals 
+    n = 2
+    ## traits
+    traits = [1, 2, 3, 4, 5]
+    ## variants
+    variants = [1, 2, 3, 4]
+
+    ## store information in matrix
+    pop = zeros(Int64, n, length(traits))
+    ## randomly asign variants to traits for n individuals
+    for i in 1:n 
+
+        pop[i,:] = rand(variants, 5)
+    
+    end
+
+    
+
+
