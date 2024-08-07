7f14d7ccc0a748736efbfc75e260b36cc44c3f84
Patrick Lauer <91125326+PatrickLa98@users.noreply.github.com>
2024-08-07 14:33:53 +0200

    wrote down steps for simulation

--- Yeh_Fogarty_Kandler_2019/Simulation_Yeh_etal_2019.jl
+++ Yeh_Fogarty_Kandler_2019/Simulation_Yeh_etal_2019.jl
@@ -1,25 +1,55 @@
+#=
+1. Initialize population with random variants at all traits and no links
+2. Iterate through timesteps
+    * individuals simultanously chose interaction partner 
+    * randomly pick a trait to copy from this partner 
+    * if trait is linked the focal individuals copies links with propability 1 - b
+    * copying successful with propability c (unsuccessful = id keeps own variant)
+    * after transmission new links form at rate of association a (between any unlinked traits)
+    * each trait can be innovated with probability μ (switch to another variant)
+3. Burn in period of 5000 timesteps with unbiased transmission
+4. additional 2000 timesteps with:
+    * unbiased transmission
+    * pay- off biased transmission
+    * conformity-biased (only one trait modeled no burn in needed)
+        
+record variant frequencies at each timestep
+
+=#
+
+
+
 using Plots
 using Distributions
 
 
-## set up
+## 1. Initialize population
 
-    ## individuals 
-    n = 2
-    ## traits
-    traits = [1, 2, 3, 4, 5]
-    ## variants
-    variants = [1, 2, 3, 4]
+## individuals 
+n = 10
+## traits
+traits = [1, 2, 3, 4, 5]
+## variants
+variants = [1, 2, 3, 4]
 
-    ## store information in matrix
-    pop = zeros(Int64, n, length(traits))
-    ## randomly asign variants to traits for n individuals
-    for i in 1:n 
+## store information in matrix
+pop = zeros(Int64, n, length(traits))
+## randomly asign variants to traits for n individuals
+for i in 1:n 
 
-        pop[i,:] = rand(variants, 5)
-    
-    end
+    pop[i,:] = rand(variants, 5)
+        
+end
 
-    
+## 2. Iterate through timesteps
+
+
+        
+
+
+
+
+
+
 
 
