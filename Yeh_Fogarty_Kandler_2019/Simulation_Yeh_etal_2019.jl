1d5c99a5c31570a921b68614ec7c8ad37d11ea0a
Patrick Lauer <91125326+PatrickLa98@users.noreply.github.com>
2024-08-07 14:50:02 +0200

    updated steps for simulation

--- Yeh_Fogarty_Kandler_2019/Simulation_Yeh_etal_2019.jl
+++ Yeh_Fogarty_Kandler_2019/Simulation_Yeh_etal_2019.jl
@@ -1,14 +1,15 @@
 #=
-1. Initialize population with random variants at all traits and no links
-2. Iterate through timesteps
+1. Initiate population with random variants at all traits and no links
+2. Initiate array, or list of matrices for links (0 1 storage)
+3. Iterate through timesteps
     * individuals simultanously chose interaction partner 
     * randomly pick a trait to copy from this partner 
     * if trait is linked the focal individuals copies links with propability 1 - b
     * copying successful with propability c (unsuccessful = id keeps own variant)
     * after transmission new links form at rate of association a (between any unlinked traits)
     * each trait can be innovated with probability μ (switch to another variant)
-3. Burn in period of 5000 timesteps with unbiased transmission
-4. additional 2000 timesteps with:
+4. Burn in period of 5000 timesteps with unbiased transmission
+5. additional 2000 timesteps with:
     * unbiased transmission
     * pay- off biased transmission
     * conformity-biased (only one trait modeled no burn in needed)
@@ -23,7 +24,7 @@ using Plots
 using Distributions
 
 
-## 1. Initialize population
+## 1. Initiate population
 
 ## individuals 
 n = 10
@@ -41,7 +42,12 @@ for i in 1:n
         
 end
 
-## 2. Iterate through timesteps
+## 2. Initiate array for links
+
+
+
+
+## 3. Iterate through timesteps
 
 
         
